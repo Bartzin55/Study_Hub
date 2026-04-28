afdf3b38588af801d614d66632c0a6f25dab4f19
LuizSousa <luizotaviobart55@gmail.com>
2026-04-28 00:04:50 -0300

    Luiz: Folders have been reorganized to meet Django standards; links to each of the previously created HTML pages are now functional.

--- Study_Hub_App/views.py
+++ Study_Hub_App/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):  # type: ignore
+    return render(request, "Study_Hub_App/index.html")  # type: ignore
+
+
+def mainPage(request):  # type: ignore
+    return render(request, "Study_Hub_App/mainPage.html")  # type: ignore
+
+
+def overallPage(request):  # type: ignore
+    return render(request, "Study_Hub_App/overallPage.html")  # type: ignore
+
+
+def schedulePage(request):  # type: ignore
+    return render(request, "Study_Hub_App/schedulePage.html")  # type: ignore
+
+
+def signUp(request):  # type: ignore
+    return render(request, "Study_Hub_App/signUp.html")  # type: ignore
